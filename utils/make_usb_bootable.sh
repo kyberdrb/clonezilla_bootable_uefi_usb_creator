30c5c1fe3fc4340d7957548b1447e0a2e8715f4a
kyberdrb <kyberdrb@gmail.com>
2022-04-18 15:05:52 +0200

    Add fallback options

--- utils/make_usb_bootable.sh
+++ utils/make_usb_bootable.sh
@@ -14,6 +14,40 @@ PARTITION="/dev/$(cat /proc/partitions | grep "${DISK_NAME}" | grep -v "${DISK_N
 partition_label="CLONEZILLA"
 sudo fatlabel "${PARTITION}" "${partition_label}"
 
+# SYSLINUX left here as a fallback in case the USB doesn't boot
+
+#printf "%s\n" "Download lates 'syslinux' package"
+#sudo pacman --sync --downloadonly --noconfirm --cachedir "/tmp/" syslinux
+#
+#syslinux_package_in_latest_version="$(find /tmp/ -maxdepth 1 -type f -name "syslinux*" | sort | head --lines=1)"
+#sudo rm "${syslinux_package_in_latest_version}.sig"
+#
+#GROUP="$(printf "%s" "${GROUPS}" | cut --delimiter=' ' --fields=1)"
+#sudo chown ${USER}:${GROUP} "${syslinux_package_in_latest_version}"
+#7z x -y "${syslinux_package_in_latest_version}" -o/tmp/syslinux_latest
+#
+#syslinux_inner_package="$(find /tmp/syslinux_latest -type f)"
+#7z x -y "${syslinux_inner_package}" -o/tmp/syslinux_inner_package
+#
+#echo "Unmount the single partition that is now on the USB '/dev/${DISK_NAME}'"
+#PARTITION_NAME=$(cat /proc/partitions | grep "${DISK_NAME}" | rev | cut -d' ' -f1 | rev | grep -v ""${DISK_NAME}"$")
+#PARTITION_DEVICE="/dev/${PARTITION_NAME}"
+#
+#udisksctl unmount --block-device "${PARTITION_DEVICE}"
+##udisksctl mount --block-device "${PARTITION_DEVICE}"
+#
+#printf "%s\n" "Installing bootloader with 'syslinux'"
+#chmod +x "/tmp/syslinux_inner_package/usr/bin/syslinux"
+#
+#sudo "/tmp/syslinux_inner_package/usr/bin/syslinux" --install "${PARTITION_DEVICE}"
+#
+#printf "%s\n\n" "SYSLINUX bootloader successfully installed onto "${PARTITION_DEVICE}""
+#
+#sudo dd bs=440 count=1 conv=notrunc if=/tmp/syslinux_inner_package/usr/lib/syslinux/bios/gptmbr.bin of=/dev/sdb
+##sudo dd bs=440 count=1 conv=notrunc if=/tmp/syslinux_inner_package/usr/lib/syslinux/bios/mbr.bin of=/dev/sdb
+#
+#printf "%s\n\n" "Bootable GPT has been successfully flashed onto "${DISK_DEVICE}""
+
 echo "Verification"
 lsblk -o NAME,FSTYPE,LABEL,UUID "${DISK_DEVICE}"
 echo "========================================="
