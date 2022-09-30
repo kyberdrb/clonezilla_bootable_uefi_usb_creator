fa1bea32b7f7ff89ddebf1652b6f5918d388acb0
kyberdrb <kyberdrb@gmail.com>
2022-09-30 18:12:26 +0200

    Ommit setting bootable flags...
    ...in order to access the USB from file manager

--- utils/make_usb_bootable.sh
+++ utils/make_usb_bootable.sh
@@ -7,7 +7,6 @@ DISK_DEVICE="/dev/${DISK_NAME}"
 
 echo "Set the 'boot' flag"
 partition_number="$(sudo parted --machine --script "${DISK_DEVICE}" print | tail -n 1 | cut -d':' -f1)"
-sudo parted --script "${DISK_DEVICE}" set "${partition_number}" "boot" "on"
 
 echo "Set the partition name for easier recognition in the file manager and in the terminal"
 PARTITION="/dev/$(cat /proc/partitions | grep "${DISK_NAME}" | grep -v "${DISK_NAME}"$ | tr -s ' \t' | rev | cut -d' ' -f1 | rev)"
